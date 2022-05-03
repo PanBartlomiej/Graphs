6cf3b24d8cafc3c0cd208d888f3a4cd8d9078ddd
leaf <krissthompson@protonmail.com>
2022-05-03 07:22:47 +0200

    make Graf not abstract

--- src/main/java/ListaS.java
+++ src/main/java/ListaS.java
@@ -3,17 +3,17 @@ import java.util.ArrayList;
 //Lista sąsiędztwa
 public class ListaS extends Graf {
 
-    MacierzS toMacierzS(){
-    MacierzS macierzS = new MacierzS() ;
+    MacierzS toMacierzS() {
+        MacierzS macierzS = new MacierzS();
 
-    for(int i=0; i<macierz.size(); i++) {
-        macierzS.getMacierz().add(new ArrayList<>());
-        for (int j = 0; j < macierz.size(); j++)
-           if(macierz.get(i).contains(j))
-            macierzS.getMacierz().get(i).add(1);
-           else macierzS.getMacierz().get(i).add(0);
-    }
-    return macierzS;
+        for (int i = 0; i < macierz.size(); i++) {
+            macierzS.getMacierz().add(new ArrayList<>());
+            for (int j = 0; j < macierz.size(); j++)
+                if (macierz.get(i).contains(j))
+                    macierzS.getMacierz().get(i).add(1);
+                else macierzS.getMacierz().get(i).add(0);
+        }
+        return macierzS;
     }
 
 }
